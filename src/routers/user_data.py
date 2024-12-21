d8cc232e89855f50d496615c99ca2b56e15cc16a
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2024-12-21 18:04:05 +0500

    upgrade

--- src/routers/user_data.py
+++ src/routers/user_data.py
@@ -8,24 +8,6 @@ from src.database.models import PersonalData
 router = APIRouter(prefix="/user_data", tags=["user_data"])
 
 
-@router.post("/post_personal")
-async def post_personal_data(model: PersonalDataModel, request: Request):
-    tkn = request.cookies.get("access")
-    data = await token(tkn)
-    if data != None:
-        user_model = PersonalData(
-            email=data,
-            first_name=model.first_name,
-            last_name=model.last_name,
-            dad_name=model.dad_name,
-            bio=model.bio
-        )
-        await CRUD().create_data(user_model)
-        return HTTPException(status_code=status.HTTP_200_OK)
-    else:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-
-
 @router.get("/get_personal")
 async def get_personal_data(request: Request):
     tkn = request.cookies.get("access")
@@ -42,9 +24,18 @@ async def put_personal_data(model: PersonalDataModelUpdate, request: Request):
     tkn = request.cookies.get("access")
     data = await token(tkn)
     if data != None:
-        await CRUD().update_data(
-            model=PersonalData, new_model=model, email=data
-        )
-        return HTTPException(status_code=status.HTTP_200_OK)
+        if await CRUD().read_data(PersonalData, email=data) != None:
+            await CRUD().update_data(model=PersonalData, new_model=model, email=data)
+            return HTTPException(status_code=status.HTTP_200_OK)
+        else:
+            user_model = PersonalData(
+                email=data,
+                first_name=model.first_name,
+                last_name=model.last_name,
+                dad_name=model.dad_name,
+                bio=model.bio,
+            )
+            await CRUD().create_data(user_model)
+            return HTTPException(status_code=status.HTTP_200_OK)
     else:
         return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
