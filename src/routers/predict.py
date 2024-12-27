cee877dd5a963b20aa07b94763af1e173b7bd420
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2024-12-28 02:11:40 +0500

    .

--- src/routers/predict.py
+++ src/routers/predict.py
@@ -15,17 +15,17 @@ router = APIRouter(prefix="/predict", tags=["predict"])
 async def predict(model: PredictModel, request: Request):
     tkn = request.cookies.get("access")
     data = await token(tkn)
-    if data != None:
-        recomendate = await send_data_recomendate(model)
-        classifier = await send_data_classifier_applicants(
-            model, direction=recomendate["data"]
-        )
-        return {
-            "recomendate": recomendate["data"],
-            "classifier": classifier["predictions"],
+    # if data != None:
+    recomendate = await send_data_recomendate(model)
+    classifier = await send_data_classifier_applicants(
+        model, direction=recomendate["data"]
+    )
+    return {
+        "recomendate": recomendate["data"],
+        "classifier": classifier["predictions"],
         }
-    else:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    # else:
+    #     return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
 
 @router.post("/free")
