eb7396106a95b508896ba6d41dee02a29c9f9606
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2025-02-16 03:30:37 +0500

    убрал импорт url базы данных

--- src/database/database.py
+++ src/database/database.py
@@ -1,14 +1,14 @@
 from typing import Annotated
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, declared_attr, mapped_column
-from src.config import get_db_url
+# from src.config import get_db_url
 
 
-DATABASE_URL = get_db_url()
+# DATABASE_URL = get_db_url()
 
 
-engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+# engine = create_async_engine(DATABASE_URL)
+# async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
 int_pk = Annotated[int, mapped_column(primary_key=True)]
