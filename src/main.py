c5685a85c80bce151821abd20b7db0de5f7b80ed
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2025-02-27 22:21:57 +0500

    поправил корсы

--- src/main.py
+++ src/main.py
@@ -43,15 +43,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=[
-        "GET",
-        "POST",
-        "PUT",
-        "OPTIONS",
-    ],
-    allow_headers=[
-        "Content-Type",
-        "Cookie",
-    ],
+    allow_methods=["*"],
+    allow_headers=["*"],
     expose_headers=["*"],
 )
