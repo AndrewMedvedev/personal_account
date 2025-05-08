e2d4cd61a49971033d2bd9142c8a7494279707d2
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2025-05-08 18:20:51 +0500

    вернул все обратно

--- src/routers/set_token.py
+++ src/routers/set_token.py
@@ -5,31 +5,21 @@ set_token = APIRouter(prefix="/set/token", tags=["set_token"])
 
 
 @set_token.get("/{access}/{refresh}")
-async def set_tokens(access: str, refresh: str) -> JSONResponse:
-    response = JSONResponse(content={"message": "success"})
-    response.headers.append(
-        "Set-Cookie",
-        "access=" + access + "; SameSite=None; Secure; HttpOnly; Max-Age=7200; Partitioned"
+async def set_tokens(access: str, refresh: str, response: Response) -> JSONResponse:
+    response.set_cookie(
+        key="access",
+        value=access,
+        samesite="none",
+        httponly=True,
+        secure=True,
+        max_age=7200,
     )
-    response.headers.append(
-        "Set-Cookie",
-        "refresh=" + refresh + "; SameSite=None; Secure; HttpOnly; Max-Age=18000; Partitioned"
+    response.set_cookie(
+        key="refresh",
+        value=refresh,
+        samesite="none",
+        httponly=True,
+        secure=True,
+        max_age=18000,
     )
-    return response
-    # response.set_cookie(
-    #     key="access",
-    #     value=access,
-    #     samesite="none",
-    #     httponly=True,
-    #     secure=True,
-    #     max_age=7200,
-    # )
-    # response.set_cookie(
-    #     key="refresh",
-    #     value=refresh,
-    #     samesite="none",
-    #     httponly=True,
-    #     secure=True,
-    #     max_age=18000,
-    # )
-    return response
+    return JSONResponse(content={"message": "success"})
