f7d27d8fa930e7d9007dceecedffba4a45a738d6
AndrewMedvedev <andrejmedvedev2713@gmail.com>
2025-05-08 17:54:14 +0500

    изменил логику закрепления токенов

--- src/routers/set_token.py
+++ src/routers/set_token.py
@@ -1,25 +1,35 @@
 from fastapi import APIRouter, Response
-from fastapi.responses import RedirectResponse
+from fastapi.responses import JSONResponse
 
 set_token = APIRouter(prefix="/set/token", tags=["set_token"])
 
 
 @set_token.get("/{access}/{refresh}")
-async def set_tokens(access: str, refresh: str, response: Response) -> Response:
-    response.set_cookie(
-        key="access",
-        value=access,
-        samesite="none",
-        httponly=True,
-        secure=True,
-        max_age=7200,
+async def set_tokens(access: str, refresh: str) -> JSONResponse:
+    response = JSONResponse(content={"message": "success"})
+    response.headers.append(
+        "Set-Cookie",
+        "access=" + access + "; SameSite=None; Secure; HttpOnly; Max-Age=7200; Partitioned"
     )
-    response.set_cookie(
-        key="refresh",
-        value=refresh,
-        samesite="none",
-        httponly=True,
-        secure=True,
-        max_age=18000,
+    response.headers.append(
+        "Set-Cookie",
+        "refresh=" + refresh + "; SameSite=None; Secure; HttpOnly; Max-Age=18000; Partitioned"
     )
-    return RedirectResponse(url="https://online-service-for-applicants.onrender.com/profile")
+    return response
+    # response.set_cookie(
+    #     key="access",
+    #     value=access,
+    #     samesite="none",
+    #     httponly=True,
+    #     secure=True,
+    #     max_age=7200,
+    # )
+    # response.set_cookie(
+    #     key="refresh",
+    #     value=refresh,
+    #     samesite="none",
+    #     httponly=True,
+    #     secure=True,
+    #     max_age=18000,
+    # )
+    return response
